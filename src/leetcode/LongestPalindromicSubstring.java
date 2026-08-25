3cf719757b12ddc87e56022fd43eee716b49d2e2
mkrechkivskyi <misha.krechkivskyi25@gmail.com>
2026-08-26 00:42:23 +0300

    Solved LongestPalindromicSubstring task leetcode

--- src/leetcode/LongestPalindromicSubstring.java
+++ src/leetcode/LongestPalindromicSubstring.java
@@ -1,63 +1,6 @@
 package leetcode;
 
 public class LongestPalindromicSubstring {
-//
-//    public static void main(String[] args) {
-//        String ex = "tbabade";
-
-    /// /        String ex = "racecar";
-//        LongestPalindromicSubstring longestPalindromicSubstring = new LongestPalindromicSubstring();
-//        String s = longestPalindromicSubstring.longestPalindrome(ex);
-//    }
-    public static void main(String[] args) {
-        // Створюємо об'єкт класу, як у вашому прикладі
-        LongestPalindromicSubstring solver = new LongestPalindromicSubstring();
-
-        int passed = 0;
-        int failed = 0;
-
-        // Вхідні дані для тестування
-        String[] inputs = {"tbabade", "racecar",                           // Ваші два приклади
-                "babad", "cbbd", "forgeeksskeegfor", "abacaba", // Базові
-                "a", "ac", "bb", "",                            // Граничні
-                "aaaa", "ccccc", "abcdaaaaa",                   // Повторення
-                "abcdeeffgh", "abcdegegfh",                     // Парні/Непарні
-                "abacahohoh", "abcdefgfedcba", "abcbaeffe"       // Складні
-        };
-
-        // Очікувані результати
-        String[] expected = {"babad", "racecar", "bab", "bb", "geeksskeeg", "abacaba", "a", "a", "bb", "", "aaaa", "ccccc", "aaaaa", "ee", "ege", "hohoh", "abcdefgfedcba", "abcba"};
-
-        System.out.println("=== ЗАПУСК ТЕСТІВ ===\n");
-
-        for (int i = 0; i < inputs.length; i++) {
-            String input = inputs[i];
-            String expectedOutput = expected[i];
-
-            // Виклик методу через створений об'єкт
-            String actualOutput = solver.longestPalindrome(input);
-
-            // Обробка випадків, де LeetCode приймає кілька варіантів відповіді
-            boolean isCorrect = actualOutput.equals(expectedOutput);
-            if (input.equals("tbabade") && (actualOutput.equals("bab") || actualOutput.equals("aba"))) isCorrect = true;
-            if (input.equals("babad") && actualOutput.equals("aba")) isCorrect = true;
-            if (input.equals("ac") && actualOutput.equals("c")) isCorrect = true;
-
-            if (isCorrect) {
-                System.out.println("[ PASS ] Тест " + (i + 1) + ": Вхід: \"" + input + "\" -> Отримано: \"" + actualOutput + "\"");
-                passed++;
-            } else {
-                System.out.println("[ FAIL ] Тест " + (i + 1) + ": Вхід: \"" + input + "\"");
-                System.out.println("         Очікувалось: \"" + expectedOutput + "\"");
-                System.out.println("         Отримано:    \"" + actualOutput + "\"");
-                failed++;
-            }
-        }
-
-        System.out.println("\n=== РЕЗУЛЬТАТИ ===");
-        System.out.println("Успішно: " + passed + " / " + inputs.length);
-        System.out.println("Провалено: " + failed);
-    }
 
 
     public String longestPalindrome(String s) {
@@ -66,7 +9,6 @@ public class LongestPalindromicSubstring {
         if (s.length() < 2) {
             return s;
         }
-        //todo Суть рішення, Якщо взяти паліндром babab у нас є центр(ПРАЦЮЄ ДЛЯ СЛІВ ТИПУ SOS ABA, Дописати механіку для forgeeksskeegfor ss
         for (int i = 1; i < charArray.length; i++) {
             StringBuilder palindromeByCenter = findPalindromeByCenter(i, new StringBuilder().append(charArray[i]), charArray, 1);
             if (stringBuilder.length() < palindromeByCenter.length()) {
@@ -78,7 +20,6 @@ public class LongestPalindromicSubstring {
                     stringBuilder = forPairedCenter;
                 }
             }
-
         }
 
         return stringBuilder.toString();
@@ -86,14 +27,14 @@ public class LongestPalindromicSubstring {
 
     private StringBuilder findPalindromeByPairCenter(int i, StringBuilder res, char[] charArray, int offset) {
         if (i - offset - 1 >= 0 && i + offset < charArray.length) {
-            if (charArray[i - offset] == charArray[i + offset]) {
-                res.insert(0, charArray[i - offset]);
+            if (charArray[i - offset - 1] == charArray[i + offset]) {
+                res.insert(0, charArray[i - offset - 1]);
                 res.append(charArray[i + offset]);
-                return findPalindromeByCenter(i, res, charArray, offset + 1);
+                return findPalindromeByPairCenter(i, res, charArray, offset + 1);
             }
         }
 
-        return null;
+        return res;
     }
 
     private StringBuilder findPalindromeByCenter(int i, StringBuilder res, char[] charArray, int offset) {
@@ -106,6 +47,4 @@ public class LongestPalindromicSubstring {
         }
         return res;
     }
-
-
 }
